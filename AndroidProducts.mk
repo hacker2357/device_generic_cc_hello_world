0303043f458371ec00be683490eac5febeaa3911
hacker2357 <hacker2357@gmail.com>
2025-11-09 15:33:36 +0300

    -

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
 
 COMMON_LUNCH_CHOICES := \
-    cc_hello_world-eng \
-    cc_hello_world-userdebug \
-    cc_hello_world-user
+    lineage_cc_hello_world-eng \
+    lineage_cc_hello_world-userdebug \
+    lineage_cc_hello_world-user
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_cc_hello_world.mk
